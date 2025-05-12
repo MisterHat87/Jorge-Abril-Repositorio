6cb43605421dfb2b130e164f0a1c75ff4034e2bd
Abril Chacon Araya <abrilchacoaraya@gmail.com>
2025-05-11 23:45:06 -0600

    cambios

--- Moneda.java
+++ Moneda.java
@@ -3,4 +3,25 @@ public class Moneda {
     private double monto;
     private String  valorEntrada;
     private String  valorSalida;
+
+     public Moneda(double monto , String valorEntrada, String valorSalida) {
+        this.monto = monto;
+        this.valorEntrada = valorEntrada;
+        this.valorSalida = valorSalida;
+
+ 
+}
+
+public double getMonto() {
+        return monto;
+    }
+
+    public String getValorEntrada() {
+        return valorEntrada;
+    }
+
+    public String getValorSalida() {
+        return valorSalida;
+    }
+
 }
